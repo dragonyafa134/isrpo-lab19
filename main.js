51716f6960de1f69b008f42cfe2f5afc3a6e8c1c
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-03-17 12:23:54 +0300

    Step 5: loops in JavaScript

---
+++ main.js
@@ -0,0 +1,70 @@
+// for (let i = 0; i<5; i++ ){
+//         console.log(i);
+// }
+
+
+// for (let i = 0; i<11; i++){
+//     if (i % 2 == 0 ){
+//         console.log(i);
+//     }   
+// }
+// let s = 0;
+// for (let i = 0; i <= 10; i++) {
+//     s += i;
+// }
+// console.log(s); 
+
+
+
+// let conunt = 0;
+// while (conunt < 3){
+//     console.log("Count", conunt);
+//     conunt++;
+// }
+
+// let num = 5;
+// while (num ){
+//     console.log("Count", num);
+//     num--;
+// }
+
+
+
+// let doValue = 0;
+// do{
+//     console.log("value", doValue);
+//     doValue++;
+// }while(doValue < 5);
+
+
+// for (let i = 0; i < 10;i++){
+//     if (i=== 5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+// for (let i = 0; i < 10;i++){
+    
+//     if(i === 8){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+// for (let i=1 ; i<=3; i++){
+//     for (let j = 1; j <= 3; j++){
+//         console.log(`i = ${i}, j = ${j}`)
+//     }
+// }
+
+// for (let i = 0; i <= 4; i++) {
+//     let s = '';
+//     for (let j = 0; j <= i; j++) {
+//         s += '*';
+//     }
+//     console.log(s);
+// }
